38255d12e51ff367d1eac868b73d2255e0c61c12
oscarjavier199 <oscarjavier2@protonmail.com>
2022-02-24 13:10:14 -0500

    loop

---
+++ loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ **
+ ** loop is easy 
+ **
+ **/
+
+int main()
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        printf("Loop is Easy\n");
+    }
+
+        for (int i = 0; i < 7; ++i)
+    {
+        printf("Loop is fun!\n");
+    }
+        for (int i = 0; i < 3; ++i)
+    {
+        printf("Loop is Easy\n");
+    }
+    return 0;
+}
